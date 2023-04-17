8f6e6082107e22fa47e3950eaaa0e10249aedba3
valpesendorfer <v.p@gmx.at>
2023-04-17 15:51:10 +0200

    fix mypy issues

--- hdc/colors/utils.py
+++ hdc/colors/utils.py
@@ -1,8 +1,9 @@
 """HDC colors utils"""
 from itertools import tee, chain
-from typing import Iterable, Optional, Tuple
+from typing import cast, Iterable, Optional
 from pathlib import Path
 
+from .types import RGBTuple
 
 INF = float("INF")
 
@@ -14,24 +15,17 @@ def lagiter(some_iterable: Iterable) -> Iterable:
     return zip(prevs, items)
 
 
-def hex_to_rgb(x: str) -> Tuple[int]:
+def hex_to_rgb(x: str) -> RGBTuple:
     "convert HEX string to RGB tuple"
     assert x.startswith("#")
     x = x.lstrip("#")
-    return tuple((int(x[ix : ix + 2], 16) for ix in (0, 2, 4)))
+    rgb_tuple = tuple((int(x[ix : ix + 2], 16) for ix in (0, 2, 4)))
+    return cast(RGBTuple, rgb_tuple)
 
 
-def create_color_table(
-    ramp: "hdc.colors._classes.HDCDiscreteRamp", filename: Optional[str] = None
-) -> str:
+def create_color_table(ramp, filename: Optional[str] = None) -> str:
     """Create gdal compliant color table from color ramp"""
     # pylint: disable=import-outside-toplevel
-    from hdc.colors._classes import (
-        HDCDiscreteRamp,
-    )
-
-    assert isinstance(ramp, HDCDiscreteRamp)
-
     ctable = ""
 
     for e in ramp.ramp_ows:
