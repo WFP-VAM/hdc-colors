368d4e9765a47ccd52ac6da8c432b13a655b4310
valpesendorfer <v.p@gmx.at>
2023-04-17 21:51:42 +0200

    add optional nodata to colortxt; update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-function-docstring
+# pylint: disable=missing-function-docstring,redefined-outer-name
 """Tests for hdc.colors.utils"""
 from pathlib import Path
 from tempfile import TemporaryDirectory
@@ -7,6 +7,27 @@ import pytest
 
 from hdc.colors.utils import create_color_table, hex_to_rgb, lagiter
 from hdc.colors._classes import HDCDiscreteRamp
+
+
+@pytest.fixture
+def desired():
+    return """1 255 0 0
+2 0 255 0
+2 0 255 0
+3 0 0 255
+3 0 0 255
+"""
+
+
+@pytest.fixture
+def desired_nodata():
+    return """0 255 255 255 0
+1 255 0 0 255
+2 0 255 0 255
+2 0 255 0 255
+3 0 0 255 255
+3 0 0 255 255
+"""
 
 
 def test_lagiter():
@@ -21,14 +42,23 @@ def test_hex_to_rgb(hex_val, rgb_val):
     assert hex_to_rgb(hex_val) == rgb_val
 
 
-def test_create_color_table(class_input):
-    desired = "1 255 0 0\n2 0 255 0\n2 0 255 0\n3 0 0 255\n3 0 0 255\n"
+@pytest.mark.parametrize(
+    "nodata, table_out",
+    [
+        (None, "desired"),
+        (0, "desired_nodata"),
+    ],
+)
+def test_create_color_table(class_input, nodata, table_out, request):
     ct = HDCDiscreteRamp(class_input)
-    assert create_color_table(ct) == desired
+    assert create_color_table(ct, nodata=nodata) == request.getfixturevalue(table_out)
 
+
+def test_create_color_table_file(class_input, desired):
     with TemporaryDirectory() as tempdir:
+        ct = HDCDiscreteRamp(class_input)
         fn = tempdir + "/cols.txt"
-        _ = create_color_table(ct, fn)
+        _ = create_color_table(ct, filename=fn)
         fo = Path(fn)
         assert fo.exists()
         assert fo.read_text(encoding="utf-8") == desired
@@ -36,7 +66,7 @@ def test_create_color_table(class_input):
         del fo
 
         fn = tempdir + "/cols"
-        _ = create_color_table(ct, fn)
+        _ = create_color_table(ct, filename=fn)
         fo = Path(fn + ".txt")
         assert fo.exists()
         assert fo.read_text(encoding="utf-8") == desired
