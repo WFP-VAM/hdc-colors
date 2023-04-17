5ce2e849862a8dfbee65d5844580d9b13315de9d
valpesendorfer <v.p@gmx.at>
2023-04-17 14:24:42 +0200

    add `create_color_table`

--- hdc/colors/utils.py
+++ hdc/colors/utils.py
@@ -1,6 +1,8 @@
 """HDC colors utils"""
 from itertools import tee, chain
-from typing import Iterable, Tuple
+from typing import Iterable, Optional, Tuple
+from pathlib import Path
+
 
 INF = float("INF")
 
@@ -17,3 +19,36 @@ def hex_to_rgb(x: str) -> Tuple[int]:
     assert x.startswith("#")
     x = x.lstrip("#")
     return tuple((int(x[ix : ix + 2], 16) for ix in (0, 2, 4)))
+
+
+def create_color_table(
+    ramp: "hdc.colors._classes.HDCDiscreteRamp", filename: Optional[str] = None
+) -> str:
+    """Create gdal compliant color table from color ramp"""
+    # pylint: disable=import-outside-toplevel
+    from hdc.colors._classes import (
+        HDCDiscreteRamp,
+    )
+
+    assert isinstance(ramp, HDCDiscreteRamp)
+
+    ctable = ""
+
+    for e in ramp.ramp_ows:
+        v = e.get("value")
+        c = e.get("color")
+        ctable += " ".join((str(v), *map(str, hex_to_rgb(c)))) + "\n"
+
+    if filename is not None:
+        if not filename.endswith(".txt"):
+            filename += ".txt"
+        try:
+            fobj = Path(filename)
+            fobj.write_text(ctable, encoding="utf-8")
+        except OSError as exc:
+            print(
+                f"Failed to write to {filename} - does directory exists and do you have permissions for writing?"
+            )
+            raise exc
+
+    return ctable
