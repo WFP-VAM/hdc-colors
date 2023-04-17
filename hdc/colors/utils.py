0504087aae43aea33b373a986be3823c0bfaccd2
valpesendorfer <v.p@gmx.at>
2023-04-17 11:55:17 +0200

    add hex_to_rgb

--- hdc/colors/utils.py
+++ hdc/colors/utils.py
@@ -1,6 +1,6 @@
 """HDC colors utils"""
 from itertools import tee, chain
-from typing import Iterable
+from typing import Iterable, Tuple
 
 INF = float("INF")
 
@@ -10,3 +10,10 @@ def lagiter(some_iterable: Iterable) -> Iterable:
     prevs, items = tee(some_iterable, 2)
     prevs = chain([None], prevs)
     return zip(prevs, items)
+
+
+def hex_to_rgb(x: str) -> Tuple[int]:
+    "convert HEX string to RGB tuple"
+    assert x.startswith("#")
+    x = x.lstrip("#")
+    return tuple((int(x[ix : ix + 2], 16) for ix in (0, 2, 4)))
